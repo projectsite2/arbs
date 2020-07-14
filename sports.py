71133cb757ea61b42637345e729cf89776b8bfaa
stone <johnamo@gmail.com>
2020-07-14 14:42:03 +0000

    trial

---
+++ sports.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 17:29:58 2020
+
+@author: Duke Young
+"""
+
+sports= ['https://www.oddschecker.com/football/spain/copa-del-rey',  'https://www.oddschecker.com/football/english/championship',
+	'https://www.oddschecker.com/football/english/league-1', 'https://www.oddschecker.com/football/english/league-2', 'https://www.oddschecker.com/football/english/fa-cup',
+	'https://www.oddschecker.com/football/elite-coupon', 'https://www.oddschecker.com/football/france/ligue-2', 'https://www.oddschecker.com/football/champions-league',
+	'https://www.oddschecker.com/football/europa-league', 'https://www.oddschecker.com/football/womens-coupon','https://www.oddschecker.com/football/germany/bundesliga','https://www.oddschecker.com/football/italy/serie-a',
+    'https://www.oddschecker.com/football/spain/la-liga-primera','https://www.oddschecker.com/football/scottish/premiership','https://www.oddschecker.com/football/netherlands/eredivisie','https://www.oddschecker.com/football/netherlands/eerste-divisie','https://www.oddschecker.com/football/italy/serie-b',
+    'https://www.oddschecker.com/football/italy/super-cup','https://www.oddschecker.com/football/spain/la-liga-segunda','https://www.oddschecker.com/football/spain/la-liga-segunda-b','https://www.oddschecker.com/football/portugal/segunda-liga'
+    'https://www.oddschecker.com/football/poland/ekstraklasa','https://www.oddschecker.com/football/norway/eliteserien','https://www.oddschecker.com/football/poland/cup','https://www.oddschecker.com/football/sweden/allsvenskan',
+    'https://www.oddschecker.com/football/switzerland/super-league','https://www.oddschecker.com/football/wales/premier-league','https://www.oddschecker.com/football/world/australia/a-league',
+    'https://www.oddschecker.com/football/world/japan/emperors-cup','https://www.oddschecker.com/football/world/tunisia/league-1','https://www.oddschecker.com/football/world/uae/premier-league',
+    
+    'https://www.oddschecker.com/football/france/ligue-1','https://www.oddschecker.com/football/portugal/league-cup','https://www.oddschecker.com/football/belgium/jupiler-pro-league','https://www.oddschecker.com/football/belgium/division-2','https://www.oddschecker.com/football/cyprus/1-division',
+    'https://www.oddschecker.com/football/greece/superleague','https://www.oddschecker.com/football/finland/veikkausliiga','https://www.oddschecker.com/football/hungary/nb1','https://www.oddschecker.com/football/ireland/premier-division',
+    'https://www.oddschecker.com/football/scottish/championship'
+    ]  
